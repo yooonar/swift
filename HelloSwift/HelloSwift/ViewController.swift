3b175356f5db48c1adc5f3cde712202ff4162917
yooonar <aryooon33@gmail.com>
2020-05-31 18:11:26 +0900

    Hello Swift - button click print

---
+++ HelloSwift/HelloSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloSwift
+//
+//  Created by yoonar on 2020/05/31.
+//  Copyright © 2020 yoonar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // click event
+    @IBAction func ClickMoveBtn(_ sender: Any) {
+        print("Click MOVE")
+    }
+    
+
+}
+
