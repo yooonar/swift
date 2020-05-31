8f5eef383e97bc53cc01a2a4ebac992f66b7273f
yooonar <aryooon33@gmail.com>
2020-05-31 16:04:29 +0900

    BareBasic push! 2020.05.31

---
+++ BareBasic/BareBasic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BareBasic
+//
+//  Created by yoonar on 2020/05/31.
+//  Copyright © 2020 yoonar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
