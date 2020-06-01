a5f180b04e6f8444e1806c0eb301499b5ec4450f
yooonar <aryooon33@gmail.com>
2020-06-01 17:17:29 +0900

    syntax - string

---
+++ syntax/string.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+let str1 = "Hello, Swift"
+let str2 = "\"Hello\", Swift"
+print(str1)
+print(str2)
+
+let str3 = #""Hello", Swift "#
+print(str3)
+
+let str4 = "Hello, \nSwift"
+print(str4)
+
+let str5 = "Hello, \t Swift"
+print(str5)
+
