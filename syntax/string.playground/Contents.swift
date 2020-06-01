bcfacb17c2c85c28c3f666c60250ffe43aee5f62
yooonar <aryooon33@gmail.com>
2020-06-01 17:19:36 +0900

    syntax - string add comment

--- syntax/string.playground/Contents.swift
+++ syntax/string.playground/Contents.swift
@@ -5,6 +5,8 @@ let str2 = "\"Hello\", Swift"
 print(str1)
 print(str2)
 
+// string 쓸 때 \와 "를 인식 못하는 경우가 종종 있다.
+// 이 때 string 전체의 앞 뒤에 #을 붙여주면 raw string이 되어 제대로 인식한다.
 let str3 = #""Hello", Swift "#
 print(str3)
 
