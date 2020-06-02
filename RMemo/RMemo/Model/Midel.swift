80f8fe92416cb10154234c3370b8b84570b36bca
yooonar <aryooon33@gmail.com>
2020-06-02 22:28:41 +0900

    RMemo - Memo class

---
+++ RMemo/RMemo/Model/Midel.swift
@@ -0,0 +1,29 @@
+//
+//  Midel.swift
+//  RMemo
+//
+//  Created by yoonar on 2020/06/02.
+//  Copyright © 2020 yoonar. All rights reserved.
+//
+
+import Foundation
+
+// 메모 클래스 선언
+class Memo {
+    // 내용
+    var content: String
+    // 메모 추가 날짜
+    var insertDate: Date
+    
+    // 생성자(속성 초기화)
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    // 테이블에 표시할 더미 데이터를 배열에 미리 저장
+    static var dummyMemoList = [
+        Memo(content: "RAMI MEMO"),
+        Memo(content: "Thank's! 💜")
+    ]
+}
