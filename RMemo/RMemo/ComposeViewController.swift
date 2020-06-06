39c84db1d473d687e25cbde148747bc9963173a3
yooonar <aryooon33@gmail.com>
2020-06-06 23:54:34 +0900

    RMemo - cancel button event

--- RMemo/RMemo/ComposeViewController.swift
+++ RMemo/RMemo/ComposeViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ComposeViewController: UIViewController {
 
+    @IBAction func close(_ sender: Any) {
+        // dismiss: 화면을 닫을 때 효과, 보통 화면을 열 땐 모달창
+        // animate true: 화면을 닫을 때 애니메이션 효과 주는 것
+        // completion: 화면을 닫은 후 실행하기 위한 것. 기존에는 별도의 클로저 실행하지만 이번엔 아무것도 전송하지 않을 거라 nil 처리
+        dismiss(animated: true, completion: nil)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
