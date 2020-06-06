7e796c431cad827f4278d71b14a37f955432759b
yooonar <aryooon33@gmail.com>
2020-06-06 23:47:08 +0900

    RMemo - new memo display

---
+++ RMemo/RMemo/ComposeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ComposeViewController.swift
+//  RMemo
+//
+//  Created by yoonar on 2020/06/06.
+//  Copyright © 2020 yoonar. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
