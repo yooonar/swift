f9ae0de4635f2ecaba776c00536caab70c1bbeca
yooonar <aryooon33@gmail.com>
2020-06-15 16:49:22 +0900

    syntax - tool study

---
+++ MasteringSwift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, swift!"
+print(str)
+
+// CGRect: 구조체. 프레임의 위치와 너비, 높이를 조절함 값을 미리보고싶을 때 사용
+let r = CGRect(x: 0, y: 10, width: 20, height: 30)
+
+var sum = 0
+for i in 1...10 {
+    sum += i
+}
+print(sum)
