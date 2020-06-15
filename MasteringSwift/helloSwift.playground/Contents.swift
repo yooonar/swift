486232f52c8f670ed7a77fdda10910bd0b196071
yooonar <aryooon33@gmail.com>
2020-06-15 17:05:44 +0900

    syntax - annotation, print, dump

---
+++ MasteringSwift/helloSwift.playground/Contents.swift
@@ -0,0 +1,14 @@
+// framework 를 import
+import UIKit
+
+//지도와 관련된 기능을 import 하고 싶은 경우: import MapKit
+//import MapKit
+
+
+var str = "Hello, swift"
+print(str)
+dump(str)
+// single line
+/*
+ multiple line
+ */
