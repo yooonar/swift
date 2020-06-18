c67b363cdaae4711a8c6c8355730319793d62d3b
yooonar <aryooon33@gmail.com>
2020-06-18 18:04:10 +0900

    syntax - switch 3

--- MasteringSwift/switch.playground/Contents.swift
+++ MasteringSwift/switch.playground/Contents.swift
@@ -9,19 +9,19 @@ import UIKit
  switch valueExpression {
     case pattern:
         statements // 반드시 한 줄 이상 포함되어있어야 함
-    case pattern, pattern:
+    case pattern, pattern: // 여러 조건을 or 조건으로 작성 가능
         statements
     default:
         statements
  }
  */
-let num = 9
+let num = 1
 switch num {
 case 1:
     print("one")
-case 2, 3:
+case 2, 3: // 2 or 3
     print("two or three")
-default:
+default: // Switch must be exhaustive. <- switch 는 모든 경우의 수를 처리해야한다
     // break
     print("others")
     // 아무것도 안하고 싶을 땐 break를 넣는다.
@@ -55,7 +55,7 @@ let temperature = -8 // 온도
 switch temperature {
 case ..<10:
     print("cold")
-case 11...20:
+case 10...20:
     print("cool")
 case 21...27:
     print("warm")
@@ -82,7 +82,7 @@ default:
 }
 /*:
  ## Fall Through 예시
- 로그인을 구현할 때 로그인 실패를 9번까지는 그냥 경고만 출력, 10번쨰는 경고를 출력한다음 리셋, 그 이상은 경고없이 바로 리셋
+ 로그인 기능을 구현할 때 로그인 실패를 9번까지는 그냥 경고만 출력, 10번쨰는 경고를 출력한다음 리셋, 그 이상은 경고없이 바로 리셋
  중복 소스를 줄여줌
  */
 let attempts = 10
