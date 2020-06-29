6bd5b107eda2662f969748d235940186b1020f28
yooonar <aryooon33@gmail.com>
2020-06-29 17:29:34 +0900

    syntax - array 1

---
+++ MasteringSwift/array.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+/*:
+ # Array
+ 
+ ## Array Literal
+    [elem, elem, ... ]
+ */
+
+
+let nums = [1, 2, 3]
+// num ← Int type
+
+/*:
+ ## Array Type
+ Array 문법
+ ### Array<T> ← 기본 문법
+ ### [T] ← Int 배열의 단축 문법
+ */
