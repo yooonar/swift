3279a3846d5398c5dedc6451608d7e7999d9cf69
yooonar <aryooon33@gmail.com>
2020-07-01 16:25:42 +0900

    syntax - array

--- MasteringSwift/array.playground/Contents.swift
+++ MasteringSwift/array.playground/Contents.swift
@@ -34,14 +34,14 @@ import Foundation
  배열 생성 방법: 리터럴을 사용하여 생성하는 방법
  */
 
-// 배열 리터럴을 사용하여 정수 배열을 선언
+// 배열 리터럴을 사용하여 정수 배열을 선언 - 값이 있는 경우
 let numbers = [1, 2, 3]
 
 // 빈 리터럴 생성
-// let emptyArray = [] Error!
-// 배열의 자료형은 저장된 요소에 의해 지정되는데 빈 리터럴을 사용하는 경우 에러가 발생한다.
-// error msg: Empty collection literal requires an explicit type.
-// 이 때는 자료형을 직접 선언해준다 ← 변수명: [타입] Eg. emptyArray: [Int]
+// let emptyArray = [] ← Error!
+// 배열의 자료형은 저장된 요소에 의해 추론되는데 빈 리터럴을 사용하는 경우 추론할 요소가 없기 때문에 에러가 발생한다.
+// 빈 리터럴을 사용할 때는 형식 추론을 사용할 수 없고 자료형을 직접 선언해주어야 한다.  ← 변수명: [타입] Eg. emptyArray: [Int]
+// error msg: Empty collection literal requires an explicit type.ㄴ
 let emptyArray: [Int] = []
 
 // 생성자를 사용하여 배열 선언 - 정식 문법
@@ -51,7 +51,7 @@ let emptyArray2 = Array<Int>()
 let emptyArray3 = [Int]()
 
 
-// 배열을 만든다음 기본 값 채우기 이 때 repeating 생성자를 이용
+// 배열을 만든다음 기본 값 채우기. 이 때 repeating 생성자를 이용
 // repeating: 배열을 채울 기본 값 전달
 // count: 배열을 채울 요소의 숫자
 // repeating = 0, count = 10 ← 기본 값 0이 10개가 채워진 배열
@@ -65,15 +65,17 @@ print(zeroArray)
  */
 
 // 배열 요소의 숫자 확인 Int로 타입으로 리턴
-// 배열이 빈 값인지만 확인하려면 count 를 0인 경우로 체크할 수 있음
+// 배열이 빈 값인지만 확인하려면 count 를 0인 경우로 체크할 수 있음 Eg. nums.count == 0
 nums.count
+numbers.count
 
-// 빈 값 확인은 count 보다 보통 isEempty 함수를 사용하는게 더 좋다
+// 빈 값 체크는 count 보다 보통 isEempty 함수를 사용하는게 더 좋다
 nums.isEmpty
 numbers.isEmpty
 emptyArray.isEmpty
 emptyArray2.isEmpty
 emptyArray3.isEmpty
+
 
 /*:
  # Accessing Elements
@@ -121,4 +123,4 @@ emptyArray.last
 
 // subscript 를 사용하여 첫번째 요소에 접근
 // 접근할 요소가 없기 때문에 에러 발생
-// emptyArray[0]
+// emptyArray[0] // error msg. Index out of range.
