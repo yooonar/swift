185b3105484aa7d1e91f386bbd47af53169f0532
yooonar <aryooon33@gmail.com>
2020-08-03 14:13:57 +0900

    yagom syntax - overview

---
+++ syntax/yagom/overview.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+/*
+ 같은 표현식
+ Dictionary<String, Any>                            ===     [String: Any]
+ 
+ Array<Int>                                         ===     [Int]
+ 
+ Optional<Bool>                                     ===     Bool?
+ 
+ func foo() -> Void { }                             ===     func foo() { }
+ 
+ func bool(closure: (Void) -> Void) -> Void { }     ===     func bool(closure: () -> Void) { }
+ 
+ zoo(closure: { (param: Int) in print(param) } )    ===     zoo( closure: { (param) in print(param) } )
+ zoo(closure: { (param: Int) in print(param) } )    ===     zoo{ (param: Int) in print(param) }
+ zoo(closure: { (param: Int) in print(param) } )    ===     zoo{ (param) in print(param) }
+ zoo(closure: { (param: Int) in print(param) } )    ===     zoo( closure: { print(param) } )
+ zoo(closure: { (param: Int) in print(param) } )    ===     zoo { print(param) }
+ */
